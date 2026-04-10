74f16176f4995a351ebbe0a46a8fff242b601b9b
aryadavare19 <learnarya2005@gmail.com>
2026-04-10 11:17:58 +0530

    feat: Feature 6 - Heritage Badges with milestone triggers, badge shelf, and progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import FeatureDetail from "./pages/FeatureDetail"
 import Feature3Page from "./pages/Feature3Page"
+import Feature6Page from "./pages/Feature6Page"
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/feature/3" element={<Feature3Page />} />
+      <Route path="/feature/6" element={<Feature6Page />} />
       <Route path="/feature/:id" element={<FeatureDetail />} />
     </Routes>
   )
