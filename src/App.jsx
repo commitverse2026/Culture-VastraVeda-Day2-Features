8e0eb45428381773101d728fdecb59da8d3ae90b
aryadavare19 <learnarya2005@gmail.com>
2026-04-10 11:29:31 +0530

    feat: Feature 9 - Issue Tracker with report form, status tracking, and moderator dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import FeatureDetail from "./pages/FeatureDetail"
 
+import Feature9Page from "./pages/Feature9Page"
+
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+     
+      <Route path="/feature/9" element={<Feature9Page />} />
       <Route path="/feature/:id" element={<FeatureDetail />} />
     </Routes>
   )
