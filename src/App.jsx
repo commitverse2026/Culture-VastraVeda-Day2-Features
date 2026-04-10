8e27f14324ce8b3dc50dd51d7c10a258cbee3209
aksharagupta08012007 <gakshara213@gamil.com>
2026-04-10 10:37:26 +0530

    f15 done

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Home from "./pages/Home"
 import FeatureDetail from "./pages/FeatureDetail"
 import ClothingVersionControl from "./pages/ClothingVersionControl"
 import OpenAPI from "./pages/OpenAPI"
+import DebateBoard from "./pages/DebateBoard"
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/feature/8/debate" element={<DebateBoard />} />
       <Route path="/feature/12" element={<ClothingVersionControl />} />
       <Route path="/feature/15" element={<OpenAPI />} />
       <Route path="/feature/:id" element={<FeatureDetail />} />
